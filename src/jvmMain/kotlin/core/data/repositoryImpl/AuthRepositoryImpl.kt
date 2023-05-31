4d714f0ce425dee39b6dad9ba55aa78f75e2d94a
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-05-31 19:05:24 +0300

    signup/signin response & request

--- src/jvmMain/kotlin/core/data/repositoryImpl/AuthRepositoryImpl.kt
+++ src/jvmMain/kotlin/core/data/repositoryImpl/AuthRepositoryImpl.kt
@@ -2,5 +2,15 @@ package core.data.repositoryImpl
 
 import domain.repository.AuthRepository
 import io.ktor.client.*
+import login.domain.model.LoginRequest
+import login.domain.model.LoginResponse
 
-class AuthRepositoryImpl(client: HttpClient) : AuthRepository
+class AuthRepositoryImpl(client: HttpClient) : AuthRepository {
+    override suspend fun login(loginRequest: LoginRequest): LoginResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun register(loginRequest: LoginRequest): LoginResponse {
+        TODO("Not yet implemented")
+    }
+}
